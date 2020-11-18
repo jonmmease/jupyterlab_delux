e61a39cc677e2df3dfe7b66984a15d66a36fa3aa
Frédéric Collonval <fcollonval@gmail.com>
2020-11-18 14:46:31 +0100

    Set default value of trait (#7)

--- jupyterlab_{{cookiecutter.project_slug}}/jupyterlab_{{cookiecutter.project_slug}}/labapp.py
+++ jupyterlab_{{cookiecutter.project_slug}}/jupyterlab_{{cookiecutter.project_slug}}/labapp.py
@@ -5,7 +5,7 @@ from jupyterlab import labapp
 
 def main():
     os.environ['JUPYTERLAB_DIR'] = os.path.join(sys.prefix, 'share', 'jupyter', '{{ cookiecutter.project_slug }}')
-    labapp.LabApp.app_dir = os.environ['JUPYTERLAB_DIR']
+    labapp.LabApp.app_dir.default_value = os.environ['JUPYTERLAB_DIR']
     labapp.main()
     
     
--- jupyterlab_{{cookiecutter.project_slug}}/jupyterlab_{{cookiecutter.project_slug}}/labextensionapp.py
+++ jupyterlab_{{cookiecutter.project_slug}}/jupyterlab_{{cookiecutter.project_slug}}/labextensionapp.py
@@ -5,7 +5,7 @@ from jupyterlab import labextensions
 
 def main():
     os.environ['JUPYTERLAB_DIR'] = os.path.join(sys.prefix, 'share', 'jupyter', '{{ cookiecutter.project_slug }}')
-    labextensions.BaseExtensionApp.app_dir = os.environ['JUPYTERLAB_DIR']
+    labextensions.BaseExtensionApp.app_dir.default_value = os.environ['JUPYTERLAB_DIR']
     labextensions.main()
     
 if __name__ == '__main__':
